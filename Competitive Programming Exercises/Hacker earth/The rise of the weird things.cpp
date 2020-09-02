14c74dc21d8b3c2413bf1821bcc0da47b7d40973
Tan Huynh <tanht.lavamyth@gmail.com>
2020-09-02 20:56:50 +0700

    Do improve selection sort in the rise of the weird things

--- Competitive Programming Exercises/Hacker earth/The rise of the weird things.cpp
+++ Competitive Programming Exercises/Hacker earth/The rise of the weird things.cpp
@@ -2,37 +2,21 @@
 * @Author: tanht
 * @Date:   2020-09-01 20:15:07
 * @Last Modified by:   tanht
-* @Last Modified time: 2020-09-01 21:14:14
+* @Last Modified time: 2020-09-01 22:58:27
 * @Email: tanht.lavamyth@gmail.com
 */
+
+// Source: https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/practice-problems/algorithm/the-rise-of-the-weird-things-1/description/
+
 #include <iostream>
 
 using namespace std;
 
 const int AMAX = 1007;
 
-int a[AMAX], splitedA[AMAX];
+int vampiresPowers[AMAX], zombiesPowers[AMAX];
 
-int main() {
-	int N; cin >> N;
-	for (int i = 0; i < N; ++i) {
-		cin >> a[i];
-	}
-
-	int zombiesPower = 0, vampiresPower = 0, numVampiresPower = 1;
-	for (int i = 0; i < N; ++i) {
-		if (a[i] % 2) {
-			zombiesPower += a[i];
-		}
-		else {
-			vampiresPower += a[i];
-			++numVampiresPower;
-		}
-	}
-	a[N++] = zombiesPower;
-	a[N++] = vampiresPower;
-
-
+void InsertionSort(int a[], int N) {
 	for (int i = 0; i < N - 1; ++i) {
 		int idMin = i;
 		for (int j = i + 1; j < N; ++j) {
@@ -44,24 +28,36 @@ int main() {
 			swap(a[i], a[idMin]);
 		}
 	}
+}
 
+int main() {
+	int N; cin >> N;
 
+	int idZombie = 0, idVampire = 0;
+	int sumZombiePower = 0, sumVampirePower = 0;
 	for (int i = 0; i < N; ++i) {
-		cout << a[i] << " ";
-	}
-	cout << '\n';
-
-	int zombieId = 0;
-	for (int i = 0; i < N; ++i) {
-		if (a[i] % 2 == 0) {
-			splitedA[zombieId++] = a[i];
+		int input; cin >> input;
+		if (input % 2 == 0) {
+			zombiesPowers[idZombie++] = input;
+			sumZombiePower += input;
 		}
 		else {
-			splitedA[numVampiresPower++]	= a[i];
+			vampiresPowers[idVampire++] = input;
+			sumVampirePower += input;
 		}
 	}
+	zombiesPowers[idZombie++] = sumZombiePower;
+	vampiresPowers[idVampire++] = sumVampirePower;
 
-	for (int i = 0; i < N; ++i) {
-		cout << splitedA[i] << " ";
+	InsertionSort(zombiesPowers, idZombie);
+	InsertionSort(vampiresPowers, idVampire);
+
+	for (int i = 0; i < idZombie; ++i) {
+		cout << zombiesPowers[i] << " ";
+	}
+
+
+	for (int i = 0; i < idVampire; ++i) {
+		cout << vampiresPowers[i] << " ";
 	}
 }
---
+++ Competitive Programming Exercises/Hacker earth/The rise of the weird things_Improved Selection Sort.cpp
@@ -0,0 +1,70 @@
+/*
+* @Author: tanht
+* @Date:   2020-09-02 19:57:15
+* @Last Modified by:   tanht
+* @Last Modified time: 2020-09-02 20:56:12
+* @Email: tanht.lavamyth@gmail.com
+*/
+// Source: https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/practice-problems/algorithm/the-rise-of-the-weird-things-1/
+
+#include <iostream>
+
+using namespace std;
+
+
+const int AMAX = 1007;
+
+int zombiesPowers[AMAX], vampiresPowers[AMAX];
+
+void ImprovedSelectionSort(int a[], int N) {
+	for (int leftId = 0, rightId = N - 1; leftId < rightId; ++leftId, --rightId) {
+		int minId = leftId, maxId = rightId;
+		for (int i = leftId; i <= rightId; ++i) {
+			if (a[i] < a[minId]) {
+				minId = i;
+			}
+			if (a[i] > a[maxId]) {
+				maxId = i;
+			}
+		}
+
+		if (a[minId] != a[maxId]  && !(minId == leftId && maxId == rightId)) {
+			swap (a[leftId], a[minId]);
+			if (maxId == leftId) {
+				swap (a[rightId], a[minId]);
+			}
+			else {
+				swap (a[rightId], a[maxId]);
+			}
+		}
+	}
+}
+
+int main () {
+	int N; cin >> N;
+	int zombiesPowersId = 0, vampiresPowersId = 0;
+	int zombiesPowersSum = 0, vampiresPowersSum = 0;
+	for (int  i = 0; i < N; ++i) {
+		int input; cin >> input;
+		if (input % 2 == 0) {
+			zombiesPowers[zombiesPowersId++] = input;
+			zombiesPowersSum += input;
+		}
+		else {
+			vampiresPowers[vampiresPowersId++] = input;
+			vampiresPowersSum += input;
+		}
+	}
+	zombiesPowers[zombiesPowersId++] = zombiesPowersSum;
+	vampiresPowers[vampiresPowersId++] = vampiresPowersSum;
+
+	ImprovedSelectionSort(zombiesPowers, zombiesPowersId);
+	ImprovedSelectionSort(vampiresPowers, vampiresPowersId);
+
+	for (int i = 0; i < zombiesPowersId; ++i) {
+		cout << zombiesPowers[i] << " ";
+	}
+	for (int i = 0; i < vampiresPowersId; ++i) {
+		cout << vampiresPowers[i] << " ";
+	}
+}
